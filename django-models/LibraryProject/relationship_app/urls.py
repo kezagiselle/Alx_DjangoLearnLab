52b6f16839d00b5d6c72360c7ca65e92d2916c2e
kezagiselle <kezagiselle776@gmail.com>
2025-11-16 11:27:09 +0200

    other changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books
+from .views import list_books, UserLoginView, UserLogoutView, UserRegisterView
 from relationship_app import views
 
 app_name = 'relationship_app'
@@ -9,5 +9,9 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     # Class-based view: Library detail
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    # Authentication views
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from .models import Book
 from .models import Library
 
@@ -23,3 +27,28 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+# User Authentication Views
+class UserLoginView(LoginView):
+    """
+    Class-based view for user login using Django's built-in LoginView.
+    """
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+
+class UserLogoutView(LogoutView):
+    """
+    Class-based view for user logout using Django's built-in LogoutView.
+    """
+    template_name = 'relationship_app/logout.html'
+
+
+class UserRegisterView(CreateView):
+    """
+    Class-based view for user registration using Django's UserCreationForm.
+    """
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('relationship_app:login')
