ef7c5a0037485f5b414dc4d3775c3f67859dec15
kezagiselle <kezagiselle776@gmail.com>
2025-11-16 11:10:09 +0200

    correction

--- Introduction_to_Django/LibraryProject/relationship_app/views.py
+++ Introduction_to_Django/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library
 
-# Create your views here.
+
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    """
+    Class-based view that displays details for a specific library,
+    listing all books available in that library.
+    """
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
