b0b1609fccef90e208941958ebac4cf23b6acb74
kezagiselle <kezagiselle776@gmail.com>
2025-12-19 16:39:09 +0200

    task

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -18,7 +18,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
