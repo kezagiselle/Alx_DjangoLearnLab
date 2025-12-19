3313a6b480a813d96eb49aac0f185dd6f934b640
kezagiselle <kezagiselle776@gmail.com>
2025-12-19 17:00:31 +0200

    second update

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -20,3 +20,13 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
